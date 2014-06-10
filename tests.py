148fc97860c2c614aae01ccc71cb91b17b36afa0
Odd Rune <oddrunesl@gmail.com>
2014-06-10 12:33:36 +0200

    skeletons

---
+++ tests.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestGA(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_fitness(self):
+        self.assertEqual(1,1)
+
+if __name__ == '__main__':
+    unittest.main()
