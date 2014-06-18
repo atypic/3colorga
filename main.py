07865958c798fc97c75ed527b03647e09b59b820
Odd Rune <oddrunesl@gmail.com>
2014-06-18 14:59:40 +0200

    added main run file

---
+++ main.py
@@ -0,0 +1,18 @@
+import ga
+
+import emEvolvableMotherboard
+from ttypes import *
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+
+transport = TSocket.TSocket('129.241.102.247', 9090)
+transport = TTransport.TBufferedTransport(transport)
+prot = TBinaryProtocol.TBinaryProtocol(transport)
+cli = emEvolvableMotherboard.Client(prot)
+transport.open()
+cli.ping()
+ga.gaLoop(cli, 1000)
+transport.close()
